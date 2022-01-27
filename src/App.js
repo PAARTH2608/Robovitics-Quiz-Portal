2b8b96a0d03313e5b8a5fc70cff1971a091729c7
Paarth <paarth.jain2020@vitstudent.ac.in>
2022-01-28 00:24:44 +0530

    submit addd

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import { Route, Routes } from "react-router";
+import Submit from './components/Submit';
 function App() {
   return (
     <>
     <Routes>
         {/* <Route exact path="/" element={<Start/>} />
         <Route path="/rules" element={<Authentication/>} />
-        <Route path="/domains" element={<RegisterOne/>} />
-        <Route path="/submit" element={<RegisterTwo/>} />
-        <Route path="/finish" element={<FAQ/>} /> */}
+        <Route path="/domains" element={<RegisterOne/>} /> */}
+        <Route path="/submit" element={<Submit/>} />
+        {/* <Route path="/finish" element={<FAQ/>} /> */}
       </Routes>
     </>
   );
---
+++ src/components/Submit.js
@@ -0,0 +1,6 @@
+
+const Submit = () => {
+    return <div>hi</div>;
+}
+
+export default Submit;
