dd822f8fb99257d707fe569c1c325977e0ce833b
Paarth <paarth.jain2020@vitstudent.ac.in>
2022-01-28 00:20:34 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Route, Routes } from "react-router";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Routes>
+        <Route exact path="/" element={<Start/>} />
+        <Route path="/rules" element={<Authentication/>} />
+        <Route path="/domains" element={<RegisterOne/>} />
+        <Route path="/submit" element={<RegisterTwo/>} />
+        <Route path="/finish" element={<FAQ/>} />
+      </Routes>
+    </>
   );
 }
 
