5c23e46f8edc903eb795a29f7c45c1b1a4f77269
Paarth <paarth.jain2020@vitstudent.ac.in>
2022-01-28 18:57:27 +0530

    files added

---
+++ src/components/Domains.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const Domains = () => {
+    return <div></div>;
+}
+
+export default Domains;
---
+++ src/components/Rules.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const Rules = () => {
+    return <div></div>;
+}
+
+export default Rules;
