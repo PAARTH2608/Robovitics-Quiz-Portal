d4ed200053f57538797dd3b72e6d00bcf4890f74
Paarth <paarth.jain2020@vitstudent.ac.in>
2022-02-03 09:06:50 +0530

    fix: socials

--- src/components/utils/Socials.js
+++ src/components/utils/Socials.js
@@ -21,41 +21,27 @@ const Div = styled.div`
 const Socials = () => {
   return (
     <Div>
-      <Link to="https://www.youtube.com/channel/UCFiwOI-W5b06NweratR-RdA">
-        <a target="_blank">
-          <FaYoutube size={25} style={{ color: "white" }} />
-        </a>
-      </Link>
-      <Link to="https://github.com/RoboVITics">
-        <a target="_blank">
-          <FaGithub size={25} style={{ color: "white" }} />
-        </a>
-      </Link>
-      {/* <Link to="/">
-        <a target="_blank">
-          <FaGlobe size={25} style={{ color: "white" }} />
-        </a>
-      </Link>
-      <Link to="/">
-        <a target="_blank">
-          <FaMailBulk size={25} style={{ color: "white" }} />
-        </a>
-      </Link> */}
-      <Link to="https://www.linkedin.com/company/robovitics/">
-        <a target="_blank">
-          <FaLinkedin size={25} style={{ color: "white" }} />
-        </a>
-      </Link>
-      <Link to="https://www.facebook.com/robovitics">
-        <a target="_blank">
-          <FaFacebookF size={25} style={{ color: "white" }} />
-        </a>
-      </Link>
-      <Link to=" https://instagram.com/robovitics?utm_medium=copy_link">
-        <a target="_blank">
-          <FaInstagram size={25} style={{ color: "white" }} />
-        </a>
-      </Link>
+      <a
+        target="_blank"
+        href="https://www.youtube.com/channel/UCFiwOI-W5b06NweratR-RdA"
+      >
+        <FaYoutube size={25} style={{ color: "white" }} />
+      </a>
+      <a target="_blank" href="https://github.com/RoboVITics">
+        <FaGithub size={25} style={{ color: "white" }} />
+      </a>
+      <a target="_blank" href="https://www.linkedin.com/company/robovitics/">
+        <FaLinkedin size={25} style={{ color: "white" }} />
+      </a>
+      <a target="_blank" href="https://www.facebook.com/robovitics">
+        <FaFacebookF size={25} style={{ color: "white" }} />
+      </a>
+      <a
+        target="_blank"
+        href="https://instagram.com/robovitics?utm_medium=copy_link"
+      >
+        <FaInstagram size={25} style={{ color: "white" }} />
+      </a>
     </Div>
   );
 };
